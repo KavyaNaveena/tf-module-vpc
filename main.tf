643e586564e0993a9d6163bcc85178d37c48f50e
Kavya Naveena <karumurikavya@gmail.com>
2023-04-01 11:47:08 +0530

    update

--- main.tf
+++ main.tf
@@ -9,15 +9,13 @@ resource "aws_vpc" "main" {
 
 ## public subnets
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  for_each = var.public_subnets
-  cidr_block = each.value["cidr_block"]
-  availability_zone = each.value["availability_zone"]
+  for_each          = var.public_subnets
+  cidr_block        = each.value["cidr_block"][0] + each.value["cidr_block"][1]
+  availability_zone = each.value["availability_zone"][0]+each.value["availability_zone"][1]
   tags = merge(
     var.tags,
-    { Name = "${var.env}-${each.value["name"]}" }
+    { Name = "${var.env}-${each.value["name"][0]+each.value["name"][1]}" }
   )
-
-
 }
