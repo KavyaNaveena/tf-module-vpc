135ed8da34afa4ed6b487bb42bebbd8eaf8e8078
Kavya Naveena <karumurikavya@gmail.com>
2023-04-01 22:14:51 +0530

    update

--- main.tf
+++ main.tf
@@ -35,7 +35,8 @@ resource "aws_route_table" "public_route_table" {
 ##public route table association to public subnet
 resource "aws_route_table_association" "public_association" {
   for_each = var.public_subnets
-  subnet_id      = aws_subnet.public_subnets[each.value["cidr_block"]].id
+  subnet_id = lookup(lookup(aws_subnet.public_subnets,each.value["name"],null),'id',null)
+  #subnet_id      = aws_subnet.public_subnets[each.value["name"]].id
   route_table_id =aws_route_table.public_route_table[each.value["name"]].id
 }
 
