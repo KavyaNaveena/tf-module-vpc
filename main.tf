be5bda084abd961ecb536f202cfd2cb85cff143b
Kavya Naveena <karumurikavya@gmail.com>
2023-04-01 11:54:40 +0530

    update

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
 
   for_each          = var.public_subnets
-  cidr_block        = each.value["cidr_block"][0] + each.value["cidr_block"][1]
-  availability_zone = each.value["availability_zone"][0]+each.value["availability_zone"][1]
+  cidr_block        = each.value["cidr_block"]["0"] + each.value["cidr_block"]["1"]
+  availability_zone = each.value["availability_zone"]["0"]+each.value["availability_zone"]["1"]
   tags = {
-    name = each.value["name"][0] + each.value["name"][1]
+    name = each.value["name"]["0"] + each.value["name"]["1"]
   }
 }
