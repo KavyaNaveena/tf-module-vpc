f7d1c6e38a2b8e4f586065a18a666257f593886a
Kavya Naveena <karumurikavya@gmail.com>
2023-04-07 10:26:19 +0530

    update

--- main.tf
+++ main.tf
@@ -108,7 +108,8 @@ resource "aws_route_table" "private_route_table" {
   for_each = var.private_subnets
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-",each.value["name"])[1]}"].id
+    #nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-",each.value["name"])[1]}"].id
+    nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
   }
   tags = merge(
     var.tags,
