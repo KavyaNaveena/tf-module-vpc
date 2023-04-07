79e5af157c8579cb81d14bb9846ee036845ac668
Kavya Naveena <karumurikavya@gmail.com>
2023-04-07 10:33:37 +0530

    update

--- main.tf
+++ main.tf
@@ -102,14 +102,17 @@ resource "aws_subnet" "private_subnets" {
 }
 
 ## private route-table
-resource "aws_route_table" "private_route_table" {
+resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.main.id
 
   for_each = var.private_subnets
   route {
-    cidr_block = "0.0.0.0/0"
-    #nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-",each.value["name"])[1]}"].id
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
+  }
+  route {
+    cidr_block                = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
   tags = merge(
     var.tags,
@@ -122,7 +125,7 @@ resource "aws_route_table_association" "private_association" {
   for_each = var.private_subnets
   #subnet_id = lookup(lookup(aws_subnet.public_subnets,each.value["name"],null),'id',null)
   subnet_id      = aws_subnet.private_subnets[each.value["name"]].id
-  route_table_id =aws_route_table.private_route_table[each.value["name"]].id
+  route_table_id =aws_route_table.private-route-table[each.value["name"]].id
 }
 
 ## Route to the default VPC for peering to work.
